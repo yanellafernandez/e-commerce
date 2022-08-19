caff3650528e11b464484ea91c897e4c074f6f49
yanellafernandez <fernandezyanella64@gmail.com>
2022-08-18 21:41:11 -0300

    ultimos cambios login

--- js/login.js
+++ js/login.js
@@ -6,14 +6,12 @@ function login (){
     if (usuario.mail != "" && usuario.contraseña != ""){
         localStorage.setItem('item', JSON.stringify(usuario));
         location.href = "index.html";
-    } else {
-        alert("¡Usuario y clave son requeridos!");
-    }
+    } 
 }
 
 document.addEventListener('DOMContentLoaded', ()=>{
     document.getElementById('formulario').addEventListener('submit', (event)=>{
-     // event es un parametro que recibe submit, que tiene todos los eventos de ese submit, luego usamos el preventDefault que evita la propagacion del submit(es decir enviar el formulario por naturaleza de html)
+     // event es un parametro que recibe submit, que tiene todos los eventos de ese submit, luego usamos el preventDefault que evita la propagacion del submit(es decir enviar el formulario por naturaleza de html)(previene el comportamiento por defecto)
      event.preventDefault()
         login();
     })
